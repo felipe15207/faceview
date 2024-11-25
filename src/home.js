732b27f5e81453935d6dfd147213fb2276db883c
Pedro Felipe <167935817+felipe15207@users.noreply.github.com>
2024-11-24 22:41:52 -0300

    teste 4

---
+++ src/home.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    initialSlide: 2,
+    speed: 600,
+    preventClicks: true,
+    slidesPerView: "3",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 80,
+        depth: 350,
+        modifier: 1,
+        slideShadow: true,
+    },
+    on: {
+        click(event) {
+            swiper.slideTo(this.clickedIndex);
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2000,
+    },
+})
