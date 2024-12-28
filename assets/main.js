248ca65e993b957729846e082b93f7a8d4aa2a07
Pedro Felipe <167935817+felipe15207@users.noreply.github.com>
2024-12-28 00:50:44 -0300

    correções de responsividade e adição do footer

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,18 @@
 const dfMessenger = document.querySelector('df-messenger');
-            dfMessenger.addEventListener('df-messenger-loaded', function () {
-                // Handle event
-                let dfIcon = dfMessenger?.shadowRoot?.querySelector("#widgetIcon");
-                if(dfIcon) {
-                    dfIcon.style.bottom = "70px";
-                }
-            });
+dfMessenger.addEventListener('df-messenger-loaded', function () {
+    // Handle event
+    let dfIcon = dfMessenger?.shadowRoot?.querySelector("#widgetIcon");
+    if (dfIcon) {
+        dfIcon.style.bottom = "70px";
+    }
+});
+
+const btnTopo = document.getElementById('botoes');
+
+btnTopo.addEventListener('click', function () {
+    
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
